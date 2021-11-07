f05e219f4a9e7c7fb332e147d13b859e219ec4ab
shiiyan <yan39123@gmail.com>
2021-11-08 00:03:05 +0900

    bake articles a

---
+++ tests/Fixture/TagsFixture.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * TagsFixture
+ */
+class TagsFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'title' => 'Lorem ipsum dolor sit amet',
+                'created' => '2021-11-07 14:20:20',
+                'modified' => '2021-11-07 14:20:20',
+            ],
+        ];
+        parent::init();
+    }
+}
